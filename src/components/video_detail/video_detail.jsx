3a8e17eefc2e1a4d5bbbe7a35cff238663af38a7
myplacecoffee <74812757+myplacecoffee@users.noreply.github.com>
2022-06-07 01:47:38 +0900

    commit

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import styles from './video_detail.module.css';
 
-const VideoDetail= ({video,channelPlaylist}) => (
-    <section className={styles.detail}>
-        <iframe 
-            className={styles.video}
-            title="youtube video player"
-            type="text/html" 
-            width="100%"    
-            height="500px"
-            src={`https://www.youtube.com/embed/${video.id}`}
-            frameBorder="0" 
-            allowFullScreen>
-        </iframe>
-        <h2>{video.snippet.title}</h2>
-        <span className={styles.channelTitle} onClick={()=>{channelPlaylist(video.snippet.channelId)}}>{video.snippet.channelTitle}</span>
-        <pre className={styles.description}>{video.snippet.description}</pre>
-    </section>
-);
+const VideoDetail= ({video,channelPlaylist}) => {
+    // console.log(video)
+    return (
+        <section className={styles.detail}>
+            <iframe 
+                className={styles.video}
+                title="youtube video player"
+                type="text/html" 
+                width="100%"    
+                height="500px"
+                src={`https://www.youtube.com/embed/${video.id}`}
+                frameBorder="0" 
+                allowFullScreen>
+            </iframe>
+            <h3>{video.snippet.title}</h3>
+            <span className={styles.channelTitle} onClick={()=>{channelPlaylist(video.snippet.channelId)}}>{video.snippet.channelTitle}</span>
+            <pre className={styles.description}>{video.snippet.description}</pre>
+        </section>
+    )
+};
 
 export default VideoDetail;
--- src/service/youtube.js
+++ src/service/youtube.js
@@ -7,26 +7,9 @@ class Youtube{
         };
     }
 
-    // mostPopular(){    
-    //     return fetch(
-    //         `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`, 
-    //         this.getRequestOptions)
-    //     .then(response => response.json())
-    //     .then(result => result.items);
-    // }
-
-    // search(query){
-    //     return fetch(
-    //         `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`, 
-    //         this.getRequestOptions)
-    //     .then(response => response.json())
-    //     .then(result=>result.items.map(item=>({...item, id:item.id.videoId})))
-    //     // .then(items => setVideos(items))
-    // }
-
     async channelPlaylist(channelId){
         const response = await fetch(`https://www.googleapis.com/youtube/v3/playlists?part=snippet&maxResults=25&channelId=${channelId}&key=${this.key}`,
-            this.requestOptions);
+            this.getRequestOptions);
         const result = await response.json();
         return result.items;
     }
